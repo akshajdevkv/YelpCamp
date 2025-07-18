d4370b74d28d3898c76aa3ad003c95e18f248d23
Akshaj Dev <akshajdevkv@gmail.com>
2025-07-18 18:53:10 +0530

    implement delete reviews

--- server.js
+++ server.js
@@ -115,6 +115,12 @@ app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     }
     res.redirect(`/campgrounds/${campground._id}`);
 }));
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
