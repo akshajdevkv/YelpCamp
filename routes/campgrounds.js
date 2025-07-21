c57142c71de836962559bf8324b31918974f4b14
Akshaj Dev <akshajdevkv@gmail.com>
2025-07-21 15:07:05 +0530

    add session functionality

---
+++ routes/campgrounds.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const {campgroundSchema} = require('../schemas');
+
+const validateCampground = (req, res, next) => {
+    
+    const {error} = campgroundSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400);
+    }
+    next();
+}
+router.get('/', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}));
+
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+router.post('/', validateCampground, catchAsync(async (req, res) => {
+    
+    const campground = new Campground(req.body);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        throw new ExpressError('Campground not found', 404);
+    }
+    console.log(campground);
+    res.render('campgrounds/show', { campground });
+}));
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        throw new ExpressError('Campground not found', 404);
+    }
+    res.render('campgrounds/edit', { campground });
+}));
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    if (!campground) {
+        throw new ExpressError('Campground not found', 404);
+    }
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    if (!campground) {
+        throw new ExpressError('Campground not found', 404);
+    }
+    res.redirect('/campgrounds');
+}));
+
+module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const { reviewSchema } = require('../schemas');
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400);
+    }
+    next();
+}
+
+router.post("/", validateReview, catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        throw new ExpressError('Campground not found', 404);
+    }
+    const review = new Review({
+        body: req.body.review.body,
+        rating: parseInt(req.body.review.rating)
+    });
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+module.exports = router;
