682f18354a2762bbce26ce82df384e255760a06b
Akshaj Dev <akshajdevkv@gmail.com>
2025-07-22 22:54:09 +0530

    add login page

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", catchAsync(async (req, res, next) => {
+   try{
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
@@ -17,6 +18,21 @@ router.post("/register", catchAsync(async (req, res, next) => {
         req.flash('success', 'Welcome to Yelp Camp!');
         res.redirect('/campgrounds');
     });
+   }
+   catch (e) {
+    req.flash('error', e.message);
+    res.redirect('register');
+   }
 }));
+router.get("/login", (req, res) => {
+    res.render("users/login");
+});
+router.post("/login",passport.authenticate('local',{
+    failureFlash: true,
+    failureRedirect: '/login'
+}),(req,res)=>{
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
