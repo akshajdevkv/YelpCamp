812cf89c432226d84f8b5dde365a464feb116935
Akshaj Dev <akshajdevkv@gmail.com>
2025-07-23 22:01:18 +0530

    implement cloudinary image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,14 +4,13 @@ const campgrounds = require("../controllers/campground");
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 const multer = require('multer');
- 
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.array('image'), (req, res) => {
-        console.log(req.body, req.file);
+    .post(isLoggedIn, upload.array('campground[image]'), (req, res) => {
+        console.log(req.body, req.files);
         res.send('It worked');
     });
 
